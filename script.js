af24dd64ddbec59c847cc1484bf2fb32cd0f679e
Melk-Gonçalves <melkgoncalvesofficial@gmail.com>
2025-06-15 13:42:53 -0300

    adicionei uma verificação ao input que retorna somente numero e tambem formatei o valor para a moeda local com o toString

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 const amount = document.getElementById("amount")
 
+
 amount.oninput = () => {
-    
+    //Obtem o valor atual do input e remove todos os caracteres que nao sejam numeros
+    let value = amount.value.replace(/\D/g, "")
+
+
+    //Tranformar o valor em centavos
+    value = Number(value) / 100
+
+    // atualiza o valor do input com o valor limpo
+    amount.value = formatCurrencyBRL(value)
+}
+
+ function formatCurrencyBRL(value) {
+  // Formata o valor para BRL
+     value = value.toLocaleString("pt-BR", {
+         style: "currency", 
+         currency: "BRL",
+    })
+
+    // Retorna o valor formatado
+    return value
+
 }
